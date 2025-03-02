d9defc8deb3752f49e452e92edb100d8e8430763
gitoutkai <kaihehau@hawaii.edu>
2025-03-01 17:41:33 -1000

    Create kaivariablesandtypes.py
    Please tell me that I put this on the right branch?

---
+++ kaivariablesandtypes.py
@@ -0,0 +1,58 @@
+a= 10000
+b= 3
+c= 0
+
+sum_of_ab= a + b
+
+print(sum_of_ab)
+
+result_int= a +b
+print(result_int)
+
+printing a comment on this string because it is where the code was, was, was throwing an error about how you cannot didive by 0, I will ask my math prof about that but for now lets just say it's an undefined number and leave it there.
+dividend_of_ab=a/ b
+print(dividend_of_ab)
+
+#really study math you can find solutions in certain situtations but I really don't understand them or why they make
+#sense.
+
+difference_of_ab= a -b
+
+print(difference_of_ab)
+
+
+dividend_of_ab=a / b
+
+print(dividend_of_ab)
+
+print("This is the numerical float")
+
+result_float= a /b
+print(result_float)
+
+print("This is the numerical integer")
+
+result_int= a//b
+print(result_int)
+
+multiple_of_ac= a * c
+
+print(multiple_of_ac)
+
+
+
+d = "hello"
+e= "world"
+f= "!"
+
+sum_of_def= d + e + f
+print(sum_of_def)
+
+print("this is the sum of the linguistic int")
+result_int=d+e+f
+print(result_int)
+
+
+print("This is the sum of the liguistic float")
+result_float=d +e+f
+print(result_float)
